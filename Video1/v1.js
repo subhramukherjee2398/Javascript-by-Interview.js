80fa41ed73a837956e44e4daa7186826a2d8b05e
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-06-18 20:49:47 +0530

    new folder created

---
+++ Video1/v1.js
@@ -0,0 +1,14 @@
+function getAge() {
+  "use strict";
+   age = 21;
+  console.log(age);
+}
+
+getAge();
+
+/* 
+"use strct does'nt allow any variable for Global Interntiate;so It will be refrence error fot age"
+if ue let age = 21 ; 
+so now it has a local interntiate and output will be 21
+
+*/
