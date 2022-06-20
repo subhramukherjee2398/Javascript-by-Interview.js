51345f0be69548f700021967174b559f66f53b21
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-06-21 00:02:10 +0530

    minor.

--- Video1/Video4/L3.js
+++ Video1/Video4/L3.js
@@ -16,12 +16,15 @@ when a[b] ;
 we are trying b object to a object
 now {key : b} first will be stringify and then assing to a ;
 
-so now a[object object] = 123;
+so now a[["object object"]] = 123;
 after that we are doing same thing 
 
-a[object object] = 456;
+ a[["object object"]] = 456;
 
-now a[b] & a[c] referring to the same memory location which has the vlaue of 456.
+now a[b] & a[c] referring to the same memory location 
+a[["object object"]] = 123;
+a[["object object"]] = 456;
+which has the vlaue of 456.
 
 
 */
