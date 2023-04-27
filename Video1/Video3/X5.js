bfdc96d82ed2417b43533ddb08c411f89058d8fb
Subhra Mukherjee <subhramukherjee560@gmail.com>
2023-04-28 01:05:19 +0530

    push

--- Video1/Video3/X5.js
+++ Video1/Video3/X5.js
@@ -7,11 +7,8 @@ const occ = 'Engineer'
 
 getPersonInfo`${person} is working as ${occ}`;
 
-/*
-output : ['',is working as, ''] "kiran"  "Enginner"
-In ES6 along with template literals we also have the feature to call function with template strings . 
-If the string is long we can also use rest parameters to take the arguments.
+/* It also defines two variables person and occ with string values.
 
-It divides the string where there is a variable so the first argument is displayed as an array and rest are two variables
+The last line of the code calls the getPersonInfo function using a template literal that uses the ${} syntax to interpolate the values of person and occ into the string. This syntax is called a tagged template literal, and it allows the function to be called with the interpolated string as its first argument, followed by any additional arguments passed to the function.
 
-*/
+In this case, the interpolated string is passed to the getPersonInfo function as its first argument, with no additional arguments provided. The getPersonInfo function will log the values of one, two, and three to the console, which will be the string "Kiran", the string "is working as", and the string "Engineer", respectively. */
