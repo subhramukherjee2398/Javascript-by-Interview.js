ad7b172b5780a077f48e380c03022811a84af8b5
Subhra Mukherjee <subhramukherjee560@gmail.com>
2023-04-28 01:06:31 +0530

    push

--- Video1/Video3/X5.js
+++ Video1/Video3/X5.js
@@ -11,4 +11,8 @@ getPersonInfo`${person} is working as ${occ}`;
 
 The last line of the code calls the getPersonInfo function using a template literal that uses the ${} syntax to interpolate the values of person and occ into the string. This syntax is called a tagged template literal, and it allows the function to be called with the interpolated string as its first argument, followed by any additional arguments passed to the function.
 
-In this case, the interpolated string is passed to the getPersonInfo function as its first argument, with no additional arguments provided. The getPersonInfo function will log the values of one, two, and three to the console, which will be the string "Kiran", the string "is working as", and the string "Engineer", respectively. */
+In this case, the interpolated string is passed to the getPersonInfo function as its first argument, with no additional arguments provided. The getPersonInfo function will log the values of one, two, and three to the console, which will be the string "Kiran", the string "is working as", and the string "Engineer", respectively. 
+
+[ 'Kiran', ' is working as ', '' ] Engineer undefined
+
+*/
