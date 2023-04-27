ca76b9edb4d3f56f342d84f892451d9dfe62f2c5
Subhra Mukherjee <subhramukherjee560@gmail.com>
2023-04-28 00:56:23 +0530

    push

--- Video1/Video2/p11.js
+++ Video1/Video2/p11.js
@@ -11,11 +11,20 @@
     console.log(y)
 })()
 
-/*
-The catch block receives the argument x. This is not the same x as the variable when we pass arguments. This variable x is block-scoped.
+/* This code defines an anonymous function that does the following:
 
-Later, we set this block-scoped variable equal to 1, and set the value of the variable y. Now, we log the block-scoped variable x, which is equal to 1.
+1. Declares two variables `x` and `y`, but doesn't initialize them.
+2. Uses a `try...catch` block to catch an error. 
+3. Inside the `catch` block, it declares a new variable `x` that shadows the outer `x` variable. The new `x` variable is initialized to the value `1`, and `y` is initialized to the value `2`. 
+4. The value of the inner `x` variable (`1`) is logged to the console with the string `'p'`.
+5. After the `try...catch` block, the value of the outer `x` variable is logged to the console. Since the outer `x` variable was not assigned a value, it will be `undefined`.
+6. Finally, the value of the `y` variable is logged to the console. This will be `2`, since it was assigned that value inside the `catch` block.
 
-Outside of the catch block, x is still undefined, and y is 2. When we want to console.log(x) outside of the catch block, it returns undefined, and y returns 2.
-*/
-
+So, the output of running this code will be:
+```
+1 p
+undefined
+2
+```
+Note that the `x = 1` expression inside the `catch` block is an assignment expression, not a comparison. The parentheses around it are unnecessary and have no effect on the code's behavior.
+ */
